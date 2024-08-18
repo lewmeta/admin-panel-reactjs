2f2b740dd807877a81411c739fc8f47e02e980e1
thegypsyboyo <lewisonyango9@gmail.com>
2024-08-18 14:22:05 +0300

    added footer, layout and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./pages/home/home"
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
+
+import "./styles/global.scss"
+
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+
+      ]
+    },
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
   return (
-    <div>Hello World</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span>lewmate</span>
+            <span>© Lewmate Dev Admin Dashboard</span>
+        </div>
+    )
+}
+
+export default Footer;
